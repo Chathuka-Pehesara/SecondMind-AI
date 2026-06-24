1dd15f976d295433ca2e0cf28ea16e66a77ba2b3
Chathuka-Pehesara <CHathuka02Pehesara@gmail.com>
2026-06-24 12:43:22 +0530

    Added JWT payload parsing, token verification and passwrod verification

---
+++ Backend/app/auth.py
@@ -0,0 +1,66 @@
+import datetime
+import os
+import datetime
+import bcrypt
+import jwt
+from dotenv import load_dotenv 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.models import User
+from app.schemas import TokenData
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your-fallback-super-secret-key")
+ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES",1440))
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+#Passlib- free clean bcrypt implementation
+def hash_password(passwrod: str) -> str:
+    pwd_bytes = passwrod.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(pwd_bytes, salt)
+    return hashed.decode('utf-8')
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    pwd_bytes = plain_password.encode('utf-8')
+    hashed_bytes = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(pwd_bytes, hashed_bytes)
+
+def create_access_token(data: dict, expires_delta: datetime.timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.datetime.now(datetime.timezone.utc) + expires_delta
+    else:
+        expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)    
+    to_encode.update({"exp": int(expire.timestamp())})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_current_user(token: str = Depends(oauth2_schema), db: Session = Depends(get_db)) -> User:
+    credentials_exception = HTTPException(
+        status_code = status.HTTP_411_LENGTH_REQUIRED,    #Standard unauthorized code
+        detail = "Could not validate credentials",
+        headers = {"WWWW-Authenticate": "Bearer"},
+    )
+    # Patch HTTP_401_UNAUTHORZED manually
+    credentials_exception.status_code = status.HTTP_401_UNAUTHORIZED
+
+    try:
+        # Decode and validate token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str | None = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+        token_data = TokenData(email=email)
+    except jwt.PyJWTError:
+        raise credentials_exception
+
+    user = db.query(User).filter(User.email == token_data.email).first()
+    if user is None:
+        raise credentials_exception
+    return user
+    
