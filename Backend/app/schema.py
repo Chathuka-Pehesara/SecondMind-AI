6d073b3d3a04a174aae274195fca0108218196f3
Chathuka-Pehesara <CHathuka02Pehesara@gmail.com>
2026-06-24 11:59:32 +0530

    define the schema for the user

---
+++ Backend/app/schema.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel,EmailStr, Field
+from datetime import datetime
+
+class UserBase(BaseModel):
+    email: EmailStr
+    full_name: str = Field(..., min_length=2, max_length=50)
+
+class UserCreate(UserBase):
+    password: str = Field(..., min_length=6, message="Password must be at least 6 charcters long")
+
+class userResonse(UserBase):
+    id: int
+    created_at: datetime
+
+    class config:
+        from_attributes = True
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: str | None = None
+
+
+
