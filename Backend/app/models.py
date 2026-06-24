ddccbdf1b2f9fe3cb3539087148bda326dd3436f
Chathuka-Pehesara <CHathuka02Pehesara@gmail.com>
2026-06-24 11:49:45 +0530

    Created Model for Users

---
+++ Backend/app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Nullable
+import datetime
+from sqlalchemy import column, Integer, String, DateTime
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    
