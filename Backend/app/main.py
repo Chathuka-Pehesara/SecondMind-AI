33f818e800e5fbd41b1a220c59bf649e8ba20053
Chathuka-Pehesara <CHathuka02Pehesara@gmail.com>
2026-06-25 12:08:53 +0530

    created endpoints for register, login and get user

---
+++ Backend/app/main.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from app.database import Base, engine, get_db
+from app.models import User
+from app.schemas import UserCreate, UserResponse, UserLogin, Token
+from app.auth import hash_password, verify_password, create_access_token, get_current_user
+
+# create database tables
+Base.metadata.create_all(bind=engine)
+app = FastAPI(title = "SecondMind AI Authentication Service")
+
+# setup CORS for forntend Reac integration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:5173"], # vite development URL
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+@app.post("/auth/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def register(user_data: UserCreate, db: Session = Depends(get_db)):
+    # check whether user already exists
+    existing_user = db.query(User).filter(User.email == user_data.email).first()
+    if existing_user:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "An acoount with this email address already exits"
+        )
+
+    # Hash password and create record
+    new_user = User(
+        email = user_data.email,
+        full_name = user_data.full_name,
+        hash_password = hash_password(user_data.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh()
+    return new_user
+
+@app.post("/auth/login", response_model=Token)
+def login(lognin_data=UserLogin, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.email == lognin_data.email).first()
+    if not user or not verify_password(lognin_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Incorrect email or password",
+            headers = {"WWW-AUthenticate": "Bearer"},
+        )
+    
+    # generate token
+    access_token = create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/auth/me", response_model=UserResponse)
+def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
+
