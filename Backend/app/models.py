e80dede89edf36803e754d67ed28b8794f152686
Chathuka-Pehesara <CHathuka02Pehesara@gmail.com>
2026-06-26 22:42:36 +0530

    updated model to adda conversations and message models

--- Backend/app/models.py
+++ Backend/app/models.py
@@ -1,6 +1,9 @@
+from sqlalchemy import false
+from sqlalchemy import ForeignKey
 from sqlalchemy import Nullable
 import datetime
 from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 from app.database import Base
 
 class User(Base):
@@ -11,4 +14,32 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+    # Relationships
+    conversations = relationship("conversation", back_populates="user", cascade="all, delete-orphan")
+
+class Conversations(Base):
+    __tablename__ = "Conversations"
+    
+    id = Column(String, primary_key=True, index=True)
+    user_id = Column(Tnteger, ForeignKey("user_id", ondelete="CASCADE"), nullable=False)
+    titile = Column(String, nullbase="False", default = "New Chat")
+    created_at = Column(DateTime, default = datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default = datetime.datetime.utcnow)
+
+    # Relationships
+    user = relationship("User", back_populates="Conversations")
+    message = relationship("Message", back_populates="conversations", cascade="all, delete-orphan")
+
+class Message(Base):
+    __tablename__ = "Message"
+
+    id = Column(String, primary_key=True, index=True)
+    Conversation_id = Column(String, ForeignKey("conversations.id", ondelete="CASCADE"), nullable=False)
+    role = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow) 
+
+    # Relationships
+    Conversations = relationships("Conversation", back_populates="message")
     
